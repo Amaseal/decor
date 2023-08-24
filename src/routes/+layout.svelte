c372356c5728e81d20606b10e724d23cbe9cf02c
arturs jersovs <freelance.jershov@gmail.com>
2023-08-24 22:55:30 +0300

    test

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,5 @@
 <script>
 	import '../styles.css';
-	import Header from '$lib/app/header/Header.svelte';
-	import Footer from '$lib/app/footer/Footer.svelte';
-	export let data;
 </script>
 
-<Header categories={data.header.data.categories} />
 <slot />
-<Footer categories={data.header.data.categories} />
--- src/routes/[slug]/info/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { STRAPI } from '$env/static/private';
-export const load = async ({ params }) => {
-    const data = await fetch(`${STRAPI}/api/categories/?populate[0]=info_images&filters[slug][$eq]=${params.slug}`)
-    const category = await data.json()
-    return {category: category.data[0]}
-};
